df9fea173a6a2e128a94b8c7c7c5ab12deb5f91e
Тимур Платонов <gered372@gmail.com>
2025-12-05 22:50:58 +0300

    composer installed and config db updated

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => 'mysql',        // имя сервиса из docker-compose
+    'port' => 3306,
+    'dbname' => 'game_balance',
+    'user' => 'game_user',
+    'password' => 'game_password',
+    'charset' => 'utf8mb4',
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'timur/game-balance-voting',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'adffc1f880913a5b419d56f24e2361c521ff9369',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'timur/game-balance-voting' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'adffc1f880913a5b419d56f24e2361c521ff9369',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
